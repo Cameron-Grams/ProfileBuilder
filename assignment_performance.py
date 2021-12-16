bdbd436338460709b742870893efc9e6e8892b9c
Cameron-Grams <grams@umich.edu>
2021-12-16 10:12:06 -0500

    capture dtg and number of attempts

--- assignment_performance.py
+++ assignment_performance.py
@@ -1,7 +1,8 @@
 class AssignmentDetails:
-    def __init__(self, assignment_number):
+    def __init__(self, class_number, assignment_number):
+        self.class_number = class_number
         self.assignment_number = assignment_number
         self.attempts = []
-
+        self.num_attempts = 0
 
 
--- student_profile.py
+++ student_profile.py
@@ -1,5 +1,6 @@
+from datetime import datetime as dt
+from datetime import timezone
 from assignment_performance import AssignmentDetails
-
 
 class StudentProfile:
     def __init__(self, email):
@@ -7,15 +8,25 @@ class StudentProfile:
         self.classes = set()
         self.assignments = []
 
-    def enroll_class(self, class_number):
-        self.classes.add(class_number)
+    def convert_attempts(self, raw_attempt):
+        # string in milliseconds from the coursera time stamp
+        seconds_value =  int(raw_attempt)/1000
+        utc_time_obj = dt.fromtimestamp(seconds_value, tz=timezone.utc)
+        return utc_time_obj
 
     def record_assignment(self, class_number, assignment_number, raw_attempts=None):
         # convert list of raw attempts to list of datetime objects
         # create assignment object for class and assignment number
         # update with the datetime attempts
+        assignment = AssignmentDetails(class_number, assignment_number)
+
+        if raw_attempts:
+            dtg_attempts = [self.convert_attempts(attempt) for attempt in raw_attempts]
+            assignment.attempts = dtg_attempts
+            assignment.num_attempts = len(dtg_attempts)
+
         self.classes.add(class_number)
-        assignment = AssignmentDetails(assignment_number)
         self.assignments.append(assignment)
 
+        
 
